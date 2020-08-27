33b622667edcd04435862609f7958a525e252aec
Samuel Arseneault <samuel.arseneault@polymtl.ca>
2020-08-27 15:22:32 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-def capitaliser_pays(nom):
+
+
+def square_root(number: int) -> float:
     # TODO completer la fonction
-    return nom
+    return square_rooted
+
+
+def square(number: int) -> int:
+    # TODO completer la fonction
+    return squared
+
+
+def main() -> None:
+    for i in range(25):
+        print(f"Square root: {square_root(i)}, square: {square(i)}")
 
 
 if __name__ == '__main__':
-    pays = [
-        'AfghanIstan',
-        'albania',
-        'algeria',
-        'AndorRa',
-        'angolA',
-        'antigua ANd barbuda',
-        'argEntina',
-        'Armenia',
-        'austrAlia',
-        'ausTria',
-        'azerBaijaN'
-    ]
-    for i in range(len(pays)):
-        pays[i] = capitaliser_pays(pays[i])
-
-    print(pays)
+    main()
