bbd4c1c2565746018a1930facd14df2d2398ae17
Samuel Arseneault <samuel.arseneault@polymtl.ca>
2020-08-27 16:09:49 -0400

    Update test_exercice.py

--- test_exercice.py
+++ test_exercice.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import math
 import os
 import sys
 import unittest
@@ -8,66 +10,34 @@ import exercice
 
 
 class TestExercice(unittest.TestCase):
-    def setUp(self):
-        with open('./data/sample_countries.txt') as f:
-            self.countries = [name.replace('\n', '') for name in f.readlines()]
+    def setUp(self) -> None:
+        self.numbers = list(range(100))
 
-    def test_capitalize_first_letter(self):
-        names = [
-            'Angola',
-            'The Bahamas',
-            'Andorra',
-            'Antigua and Barbuda',
-            'Georgia'
-        ]
-        altered_names = [x[0].lower() + x[1:] for x in names]
-        output = list(map(exercice.capitaliser_pays, altered_names))
-        output_first_letters = list(map(lambda name: name[0], output))
-        expected_first_letters = list(map(lambda name: name[0], names))
+    def test_square(self) -> None:
+        output_exo = list(map(exercice.square, self.numbers))
+        outpur_correct = list(map(lambda x: x ** 2, self.numbers))
+        
         self.assertListEqual(
-            output_first_letters,
-            expected_first_letters,
-            'Mettre la permiere lettre en majuscule'
+            output_exo,
+            outpur_correct,
+            'Failed to calculate squares'
         )
 
-    def test_lower_case_names(self):
-        altered_names = [country.lower() for country in self.countries]
-        output = list(map(exercice.capitaliser_pays, altered_names))
+    def test_square_root(self) -> None:
+        output_exo = list(map(exercice.square, self.numbers))
+        outpur_correct = list(map(lambda x: math.sqrt(x), self.numbers))
+        
         self.assertListEqual(
-            output,
-            self.countries,
-            'Certaines lettres ne doivent pas etre en minuscules'
-        )
-
-    def test_upper_case_names(self):
-        altered_names = [country.upper() for country in self.countries]
-        output = list(map(exercice.capitaliser_pays, altered_names))
-        self.assertListEqual(
-            output,
-            self.countries,
-            'Certaines lettres ne doivent pas etre en majuscule'
-        )
-
-    def test_linking_words(self):
-        names = [
-            'Antigua and Barbuda',
-            'Bosnia and Herzegovina'
-        ]
-        altered_names = [
-            'Antigua aNd Barbuda',
-            'Bosnia ANd Herzegovina'
-        ]
-        output = list(map(exercice.capitaliser_pays, altered_names))
-        self.assertListEqual(
-            output,
-            names,
-            'Les mots de liaison doivent etre en minuscule'
+            output_exo,
+            outpur_correct,
+            'Failed to calculate square roots'
         )
 
 
 if __name__ == '__main__':
     if not os.path.exists('logs'):
         os.mkdir('logs')
+    
     with open('logs/tests_results.txt', 'w') as f:
         loader = unittest.TestLoader()
         suite = loader.loadTestsFromModule(sys.modules[__name__])
